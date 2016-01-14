caec8eb629468a491bc1a66695d1ed6fa7de3984
Brandon Wang <bdnwang@mit.edu>
2016-01-14 18:18:29 -0500

    Created page for buying services, updated router.js to display cusotm html <title>

--- overlaps/lib/router.js
+++ overlaps/lib/router.js
@@ -2,6 +2,27 @@ Router.configure({
   layoutTemplate: 'layout'
 });
 
-Router.route('/', {name: 'overlaps'});
-Router.route('/loginpage', {name: 'loginpage'});
-Router.route('/registerpage');
+Router.route('/', {
+  title: 'Home'//set title of html (displayed in tab) here to be set later (see Router.after)
+});
+
+Router.route('/loginpage', {
+  title: 'Login'
+});
+
+Router.route('/registerpage',{
+  title: 'Register'
+});
+
+Router.route('/find',{
+  title: 'Find Services'
+});
+
+
+//sets the served html page title to the title associated with the routing if there is one
+Router.after(function(){
+  if(this.route.options.title)
+  {
+    document.title = this.route.options.title + ' - Overlaps';
+  }
+})
