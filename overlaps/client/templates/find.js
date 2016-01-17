62cd6b1d150aeb548ec589de9dc91c772497e114
Yilun Du <yilundu@mit.edu>
2016-01-17 16:24:04 -0500

    Added some changes to form submision

--- overlaps/client/templates/find.js
+++ overlaps/client/templates/find.js
@@ -7,7 +7,7 @@ Template.find.helpers({
 //  var search1 = new ReactiveVar(new RegExp($(".subject").val(),'i'));
 //	var search2 = new ReactiveVar(new RegExp($(".description").val(),'i'));
 //	return classes.find({subject: search1, description: search2});
-	var search1 = new RegExp($(".subject").val(),'i');
+	var search1 = new RegExp($(".title").val(),'i');
 	var search2 = new RegExp($(".description").val(), 'i');
 	return classes.find({subject: search1, description: search2});
 	
@@ -27,13 +27,13 @@ Template.find.events({
 	
 //	search1.set(new RegExp($(".subject").val(),'i'));
 //	search2.set(new RegExp($(".description").val(), 'i'));
-	var search1 = new RegExp($(".subject").val(),'i');
+	var search1 = new RegExp($(".title").val(),'i');
 	var search2 = new RegExp($(".description").val(), 'i');
 	var json = classes.find({subject: search1, description: search2});
 	
 	$(".Test").text("");
 	 json.forEach(function(item){
-		$(".Test").append("<li> Subject:"+item.subject+"<br> Date: "+ item.createdAt+ "<br> Description: "+item.description+"<br></li>");
+		$(".Test").append("<li> Title:"+item.title+"<br> Date: "+ item.createdAt+ "<br> Description: "+item.description+"<br></li>");
 	});
 
 
--- overlaps/client/templates/home.js
+++ overlaps/client/templates/home.js
@@ -12,18 +12,24 @@
       // Get value from form element
       var user_title = $(".title").val();
       var user_subject = $(".subject").val();
+      var user_cost = $(".cost").val();
       var user_description = $(".description").val();
       // Insert a task into the collection
       
       classes.insert({
         title: user_title,
         subject: user_subject,
+        cost: user_cost,
+        teacher: this.userId, 
         description: user_description,
-        createdAt: new Date() // current time
+        createdAt: new Date(),
+        studentNumber: 0 // current time
       });
       
       $(".subject").val('');
       $(".description").val('');
+      $(".title").val('');
+      $(".cost").val('');
       // Clear form
       }
     
