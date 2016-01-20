8c37efdba4e670dcf46e3f43baa903c2daf9a6ac
justyucode <kyujustin@gmail.com>
2016-01-20 01:38:58 -0500

    onrendered added

--- overlaps/client/templates/application/layout.js
+++ overlaps/client/templates/application/layout.js
@@ -14,3 +14,14 @@
 
 
   
+Template.layout.onRendered(function () {
+ $(".dropdown-menu").prepend("<button class= 'btn btn-default btn-block' id='editprofile'> Edit Profile </button>");
+          
+  if (Router.current().route.path() === "/"){
+$("#navbartop").css("background-color", "rgba( 11, 181, 250, .09)");
+$(".navbar").css("position", "absolute");
+}else{
+  $("#navbartop").css("background-color", "rgba( 11, 181, 250, .9)");
+  $(".navbar").css("position", "relative");
+}
+});
