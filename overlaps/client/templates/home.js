ac088479572353d5176342a4dd93cb76eb4cac05
Brandon Wang <bdnwang@mit.edu>
2016-01-20 02:01:59 -0500

    changed some stuff

--- overlaps/client/templates/home.js
+++ overlaps/client/templates/home.js
@@ -1,48 +1,58 @@
 
+Template.home.helpers({
+  username : function(){
+    if(Meteor.user() && Meteor.user().profile.name)
+    {
+      return Meteor.user().profile.name;
+    }
+    else{
+      return "";
+    }
+  }
+});
 
+Template.home.events({
+"submit .class_form": function (event) {
+  // Prevent default browser form submit
+  event.preventDefault();
 
-  Template.home.events({
-    "submit .class_form": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
+  // Get value from form element
+  //if($(this).valid){
+  var user_title = $(".title").val();
+  var user_subject = $(".subject").val();
+  var user_cost = $(".cost").val();
+  var user_description = $(".description").val();
+  // Insert a task into the collection
 
-      // Get value from form element
-      //if($(this).valid){
-      var user_title = $(".title").val();
-      var user_subject = $(".subject").val();
-      var user_cost = $(".cost").val();
-      var user_description = $(".description").val();
-      // Insert a task into the collection
-
-      classes.insert({
-        title: user_title,
-        subject: user_subject,
-        cost: user_cost,
-        teacher: Meteor.user().profile.name,
-        description: user_description,
-        createdAt: new Date(),
-        studentNumber: 0, // current time
-        teacherId: Meteor.user()._id
-      });
-
-      $(".subject").val('');
-      $(".description").val('');
-      $(".title").val('');
-      $(".cost").val('');
-      alert("Form Submitted");
-     /* }
-      else{
-        alert("Form is invalid(description must be longer than 12 characters) and cost positive)!")
-      }*/
-      // Clear form
-      }
-      ,
-    "submit .search": function(event){
-        console.log("Hello!");
-        event.preventDefault();
-        Router.go("/find");
-      }
-
-
-
+  classes.insert({
+    title: user_title,
+    subject: user_subject,
+    cost: user_cost,
+    teacher: Meteor.user().profile.name,
+    description: user_description,
+    createdAt: new Date(),
+    studentNumber: 0, // current time
+    teacherId: Meteor.user()._id
   });
+
+  $(".subject").val('');
+  $(".description").val('');
+  $(".title").val('');
+  $(".cost").val('');
+  alert("Form Submitted");
+ /* }
+  else{
+    alert("Form is invalid(description must be longer than 12 characters) and cost positive)!")
+  }*/
+  // Clear form
+  }
+  ,
+"submit .search": function(event){
+    console.log("Hello!");
+    event.preventDefault();
+    Router.go("/find");
+  }
+
+
+
+});
