a691db2c20137608fb566abb14c9ed36ef6e9cb1
Yilun Du <yilundu@mit.edu>
2016-01-20 23:55:57 -0500

    Added Venmo

--- overlaps/server/servermain.js
+++ overlaps/server/servermain.js
@@ -1,5 +1,14 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    ServiceConfiguration.configurations.remove({
+  	service: "venmo"
+	});
+	ServiceConfiguration.configurations.insert({
+	  service: "venmo",
+	  clientId: "3446",
+	  scope: "make_payments",
+	  secret: "eEmfXPZgjcLFWfYJtG3hxMM4EG8PPkhG"
+	});
   });
 }
