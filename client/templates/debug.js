f3ce5a9f462e9754de496509baf6da6b6dc0b452
Brandon Wang <bdnwang@mit.edu>
2016-01-24 11:11:15 -0500

    idk

---
+++ client/templates/debug.js
@@ -0,0 +1,11 @@
+Template.debug.helpers({
+  classes: function(){
+    return JSON.stringify(classes.find().fetch(),null,2);
+  },
+  user: function(){
+    return JSON.stringify(user.find().fetch(),null,2);
+  },
+  users: function(){
+    return JSON.stringify(Meteor.users.find().fetch(),null,2);
+  }
+})
