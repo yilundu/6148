4b841c24ad28bccb9d58f8a089d354b4cd73d160
Brandon Wang <bdnwang@mit.edu>
2016-01-25 16:48:01 -0500

    debugging deleting classes

--- client/templates/classDashboardElement.js
+++ client/templates/classDashboardElement.js
@@ -5,9 +5,9 @@ Template.classDashboardElement.events({
     //remove class from class db
     bootbox.confirm(string, function(result)
     {
+
+    //if teacher confirmed cancellation of class - proceed with cancellation procedure
     if (result===true){
-      Meteor.call('removeClass', thisContext._id);
-      //classes.remove({_id: this._id});
       //unenroll all enrolled users
       var enrolledUsers = user.find().fetch();
       classes.find({}).forEach(
@@ -17,18 +17,32 @@ Template.classDashboardElement.events({
 
         }
       );
-    for(var i = 0 ; i < enrolledUsers.length ; i++){
-      var currUser = enrolledUsers[i];
-      var newClasses = currUser.classes;
-      var index = newClasses.indexOf(thisContext._id);
-      //remove class from user's class list
-      newClasses.splice(index,1);
-      Meteor.call('updateClass', currUser._id, newClasses);
-      string = "Refunded $"+cost+"for class "+classes.findOne(classid).title+"("+ classid + ") at time: "+time;
-      Meteor.call('transactionHistory', Meteor.user()._id, string);
-      //user.update(currUser._id, {$set : {classes: newClasses}});
-    }}
-    else{}
+      //go through the formerly enrolled users and update their class lists and refund their money
+      for(var i = 0 ; i < enrolledUsers.length ; i++){
+        var currUser = enrolledUsers[i];
+        var newClasses = currUser.classes;//THIS IS AN ARRAY OF CLASSIDS
+        var index = newClasses.indexOf(thisContext._id);
+        var removedClassId = newClasses[index];
+        var removedClassObject = classes.findOne(removedClassId);
+        console.log("newClasses: "+newClasses);
+        console.log("index: "+index);
+        console.log("removedClassId: "+removeClassId);
+        console.log("removedClassObject: "+removedClassObject);
+        //remove class from user's class list
+        newClasses.splice(index,1);
+        Meteor.call('updateClass', currUser._id, newClasses);
+        string = "Refunded $"+removedClassObject.newCost+"for class "+classes.findOne(classid).title+"("+ classid + ") at time: "+time;
+        Meteor.call('transactionHistory', Meteor.user()._id, string);
+        //user.update(currUser._id, {$set : {classes: newClasses}});
+      }
+
+      //finally remove the class from the classes db
+      Meteor.call('removeClass', thisContext._id);
+    }
+    else
+    {
+
+    }
   });
   }
 });
