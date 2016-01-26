536142e26246321dc7d7cf728c756ace50e8312b
justyucode <kyujustin@gmail.com>
2016-01-25 19:59:06 -0500

    me

--- client/templates/find.js
+++ client/templates/find.js
@@ -86,7 +86,7 @@ Template.find.events({
 			+"<li class= 'classliid'>"+item.title+"</li>"
 			+"<li> Description: "+item.description+"</li>"
 			+"<li> Subject: "+item.subject+"</li>"
-			+"<li> Teacher: "+ item.teacher+ "</li><li> Date: "+ item.createdAt+ "</li>"
+			+"<li> Teacher: "+ item.teacher+ "</li><li> Date: "+ moment.format(item.createdAt)+ "</li>"
 			+"<li> Students Enrolled: "+item.studentNumber+"</li>"
 			+"<li> Address: "+ item.address +"</li>"
 			+"<li> Cost: <span class='strikethrough'>$"+ item.cost +"</span>  $"+ item.newcost +"</li>"
